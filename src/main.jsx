de65316d3fa33d3f6872d05497f26a589f9167bc
Sandra <sandrasuazo96@gmail.com>
2023-12-25 21:20:55 +0100

    fix: update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,35 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.jsx";
+import store from "./core/store.js";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css";
+import { lightTheme } from "./shared/themes/lightTheme.js";
+import { darkTheme } from "./shared/themes/darkTheme.js";
+
+const persistor = persistStore(store);
+
+const getTheme = () => {
+  const prefersLightMode = window.matchMedia(
+    "(prefers-color-scheme: light)"
+  ).matches;
+  return prefersLightMode ? lightTheme : darkTheme;
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={getTheme}>
+      <CssBaseline />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+          <ToastContainer autoClose={3000} closeOnClick theme="dark" />
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
