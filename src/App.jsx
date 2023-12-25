91bf78fe649eac8f4e69488842800d0bce966920
Sandra <sandrasuazo96@gmail.com>
2023-12-25 21:24:55 +0100

    feat: create App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
-export const App = () => {
-  return <div>App</div>;
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Appbar } from "./shared/components/appbar/Appbar.jsx";
+import { Home } from "./pages/home/Home.jsx";
+import { Login } from "./pages/login/Login.jsx";
+import { Register } from "./pages/register/Register.jsx";
+import { Profile } from "./pages/profile/Profile.jsx";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
+export default App;
