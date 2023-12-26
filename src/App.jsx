082f894d4ed326fbfb94e03c12939e97851b899b
Sandra <sandrasuazo96@gmail.com>
2023-12-26 18:32:32 +0100

    fix: update app with new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Appbar } from "./shared/components/appbar/Appbar.jsx";
+import { NavBar } from "./shared/components/navbar/Navbar.jsx";
 import { Home } from "./pages/home/Home.jsx";
+import { About } from "./pages/about/About.jsx";
+import { Activities } from "./pages/activities/Activities.jsx";
 import { Login } from "./pages/login/Login.jsx";
 import { Register } from "./pages/register/Register.jsx";
 import { Profile } from "./pages/profile/Profile.jsx";
@@ -9,10 +11,12 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Appbar />
+        <NavBar />
         <Routes>
           <Route path="*" element={<Navigate to="/" />} />
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/activities" element={<Activities />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile" element={<Profile />} />
