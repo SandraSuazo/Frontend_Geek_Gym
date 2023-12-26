a6758d12cac0e87885ac61b08d551929cf87a1b6
Sandra <sandrasuazo96@gmail.com>
2023-12-26 12:55:27 +0100

    fix: add register api call

--- src/shared/services/authApiCalls.js
+++ src/shared/services/authApiCalls.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const authURL = "http://localhost:3000";
 
+export const register = async (newUser) => {
+  const result = await axios.post(`${authURL}`, newUser);
+  return result;
+};
+
 export const login = async (user) => {
   const result = await axios.post(`${authURL}/login`, user);
   return result;
