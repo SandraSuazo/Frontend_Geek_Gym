912bb00bc4bfc19094c8f5345a0a0bde77e94a9f
Sandra <sandrasuazo96@gmail.com>
2023-12-27 18:26:55 +0100

    fix: add setAuthUser in userSlice

--- src/core/slices/userSlice.js
+++ src/core/slices/userSlice.js
@@ -3,18 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
   name: "user",
   initialState: {
+    authUser: "",
     user: "",
   },
   reducers: {
+    setAuthUser: (state, action) => {
+      state.authUser = action.payload;
+    },
     setUser: (state, action) => {
       state.user = action.payload;
     },
     cleanUser: (state) => {
-      state.user = "";
+      (state.authUser = ""), (state.user = "");
     },
   },
 });
 
-export const { setUser, cleanUser } = userSlice.actions;
+export const { setAuthUser, setUser, cleanUser } = userSlice.actions;
 export const userData = (state) => state.user;
 export default userSlice.reducer;
